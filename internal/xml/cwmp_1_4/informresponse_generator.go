66757db2976fd25e571a49bb1dc7b14df7ddde4f
Mike Digdon <32070772+digdon@users.noreply.github.com>
2026-05-29 16:35:12 -0300

    First attemp at building xml programmatically

--- internal/xml/cwmp_1_4/informresponse_generator.go
+++ internal/xml/cwmp_1_4/informresponse_generator.go
@@ -1,10 +1,11 @@
 package cwmp_1_4
 
 import (
+	"bytes"
 	"cwmp-acs/internal/cwmp"
 	"cwmp-acs/internal/xml"
+	stdxml "encoding/xml"
 	"fmt"
-	"strconv"
 )
 
 func GenerateInformResponse(message cwmp.CwmpMessageInterface, namespaces map[xml.NamespaceID]xml.Namespace) (string, error) {
@@ -13,22 +14,80 @@ func GenerateInformResponse(message cwmp.CwmpMessageInterface, namespaces map[xm
 		return "", fmt.Errorf("invalid message type")
 	}
 
-	responseXML := `<soap_env:Envelope
-	xmlns:soap_env="http://schemas.xmlsoap.org/soap/envelope/"
-	xmlns:soap_enc="http://schemas.xmlsoap.org/soap/encoding/"
-	xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	xmlns:` + namespaces[xml.CWMP].Prefix + `="` + namespaces[xml.CWMP].URL + `">
-	<soap_env:Header>
-		<` + namespaces[xml.CWMP].Prefix + `:ID soap_env:mustUnderstand="1">` + informResponse.GetID() + `</` + namespaces[xml.CWMP].Prefix + `:ID>
-		<` + namespaces[xml.CWMP].Prefix + `:UseCWMPVersion soap_env:mustUnderstand="1">` + informResponse.CwmpHeader.UseCWMPVersion + `</` + namespaces[xml.CWMP].Prefix + `:UseCWMPVersion>
-	</soap_env:Header>
-	<soap_env:Body>
-		<` + namespaces[xml.CWMP].Prefix + `:InformResponse>
-			<` + namespaces[xml.CWMP].Prefix + `:MaxEnvelopes>` + strconv.Itoa(informResponse.MaxEnvelopes) + `</` + namespaces[xml.CWMP].Prefix + `:MaxEnvelopes>
-		</` + namespaces[xml.CWMP].Prefix + `:InformResponse>
-	</soap_env:Body>
-</soap_env:Envelope>`
+	var buf bytes.Buffer
+	encoder := stdxml.NewEncoder(&buf)
+	encoder.Indent("", "  ")
 
-	return responseXML, nil
+	envelopeStart := stdxml.StartElement{
+		Name: stdxml.Name{Local: namespaces[xml.SOAPENV].Prefix + ":Envelope"},
+		Attr: []stdxml.Attr{
+			{Name: stdxml.Name{Local: "xmlns:" + namespaces[xml.SOAPENV].Prefix}, Value: namespaces[xml.SOAPENV].URL},
+			{Name: stdxml.Name{Local: "xmlns:" + namespaces[xml.SOAPENC].Prefix}, Value: namespaces[xml.SOAPENC].URL},
+			{Name: stdxml.Name{Local: "xmlns:" + namespaces[xml.XSD].Prefix}, Value: namespaces[xml.XSD].URL},
+			{Name: stdxml.Name{Local: "xmlns:" + namespaces[xml.XSI].Prefix}, Value: namespaces[xml.XSI].URL},
+			{Name: stdxml.Name{Local: "xmlns:" + namespaces[xml.CWMP].Prefix}, Value: namespaces[xml.CWMP].URL},
+		},
+	}
+
+	if err := encoder.EncodeToken(envelopeStart); err != nil {
+		return "", err
+	}
+
+	headerStart := stdxml.StartElement{Name: stdxml.Name{Local: namespaces[xml.SOAPENV].Prefix + ":Header"}}
+	if err := encoder.EncodeToken(headerStart); err != nil {
+		return "", err
+	}
+
+	idStart := stdxml.StartElement{
+		Name: stdxml.Name{Local: namespaces[xml.CWMP].Prefix + ":ID"},
+		Attr: []stdxml.Attr{{Name: stdxml.Name{Local: namespaces[xml.SOAPENV].Prefix + ":mustUnderstand"}, Value: "1"}},
+	}
+	if err := encoder.EncodeElement(informResponse.GetID(), idStart); err != nil {
+		return "", err
+	}
+
+	useCWMPVersionStart := stdxml.StartElement{
+		Name: stdxml.Name{Local: namespaces[xml.CWMP].Prefix + ":UseCWMPVersion"},
+		Attr: []stdxml.Attr{{Name: stdxml.Name{Local: namespaces[xml.SOAPENV].Prefix + ":mustUnderstand"}, Value: "1"}},
+	}
+	if err := encoder.EncodeElement(informResponse.CwmpHeader.UseCWMPVersion, useCWMPVersionStart); err != nil {
+		return "", err
+	}
+
+	if err := encoder.EncodeToken(headerStart.End()); err != nil {
+		return "", err
+	}
+
+	bodyStart := stdxml.StartElement{Name: stdxml.Name{Local: namespaces[xml.SOAPENV].Prefix + ":Body"}}
+	if err := encoder.EncodeToken(bodyStart); err != nil {
+		return "", err
+	}
+
+	informResponseStart := stdxml.StartElement{Name: stdxml.Name{Local: namespaces[xml.CWMP].Prefix + ":InformResponse"}}
+	if err := encoder.EncodeToken(informResponseStart); err != nil {
+		return "", err
+	}
+
+	maxEnvelopesStart := stdxml.StartElement{Name: stdxml.Name{Local: "MaxEnvelopes"}}
+	if err := encoder.EncodeElement(informResponse.MaxEnvelopes, maxEnvelopesStart); err != nil {
+		return "", err
+	}
+
+	if err := encoder.EncodeToken(informResponseStart.End()); err != nil {
+		return "", err
+	}
+
+	if err := encoder.EncodeToken(bodyStart.End()); err != nil {
+		return "", err
+	}
+
+	if err := encoder.EncodeToken(envelopeStart.End()); err != nil {
+		return "", err
+	}
+
+	if err := encoder.Flush(); err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
 }
